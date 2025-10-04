297c905a3300ec6a960e748efe16f118ac9c62ad
avtanshgupta <avtanshgupta@microsoft.com>
2025-10-04 19:06:49 +0530

    Initial Commit

---
+++ PomodoroTimer/PomodoroTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroTimerApp.swift
+//  PomodoroTimer
+//
+//  Created by Avtansh Gupta on 04/10/25.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
